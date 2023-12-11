6b692e03fb6f015d4e46de6b021b33d1b66d862a
Sok chansamrach <sokchansamrach2022@gmail.com>
2023-12-11 14:51:17 +0700

    last update

--- loading.js
+++
@@ -1,9 +0,0 @@
-var loader = document.getElementById("preloading");
-
-// Set a duration in milliseconds (e.g., 2000 milliseconds = 2 seconds)
-var duration = 250;
-
-// Use setTimeout to delay hiding the loader
-setTimeout(function () {
-    loader.style.display = "none";
-}, duration);
